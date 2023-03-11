78f5b549ba8111692d8bb755266442556fb92aa7
BlinkDL <blinkdl@163.com>
2023-03-11 09:18:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from pynvml import *
 nvmlInit()
 gpu_h = nvmlDeviceGetHandleByIndex(0)
 ctx_limit = 2048
-desc = f'''代码链接：
+desc = f'''相关链接：
 <a href='https://github.com/BlinkDL/ChatRWKV' target="_blank" style="margin:0 0.5em">ChatRWKV</a>
 <a href='https://github.com/BlinkDL/RWKV-LM' target="_blank" style="margin:0 0.5em">RWKV-LM</a>
 <a href="https://pypi.org/project/rwkv/" target="_blank" style="margin:0 0.5em">RWKV pip package</a>
+<a href="https://zhuanlan.zhihu.com/p/609154637" target="_blank" style="margin:0 0.5em">知乎教程</a>
 '''
 
 os.environ["RWKV_JIT_ON"] = '1'
@@ -17,7 +18,6 @@ os.environ["RWKV_CUDA_ON"] = '1' # if '1' then use CUDA kernel for seq mode (muc
 
 from rwkv.model import RWKV
 model_path = hf_hub_download(repo_id="BlinkDL/rwkv-4-pile-7b", filename="RWKV-4-Pile-7B-EngChn-testNovel-1883-ctx2048-20230310.pth")
-# model_path = hf_hub_download(repo_id="BlinkDL/rwkv-4-pile-169m", filename="RWKV-4-Pile-169M-20220807-8023.pth")
 model = RWKV(model=model_path, strategy='cuda fp16')
 from rwkv.utils import PIPELINE, PIPELINE_ARGS
 pipeline = PIPELINE(model, "20B_tokenizer.json")
@@ -76,10 +76,11 @@ def infer(
 examples = [
     ["以下是不朽的科幻史诗长篇巨著，描写细腻，刻画了数百位个性鲜明的英雄和宏大的星际文明战争。\n第一章", 200, 0.9, 0.85, 0.1, 0.1],
     ["“区区", 200, 0.9, 0.85, 0.1, 0.1],
+    ["这竟然是", 200, 0.9, 0.85, 0.1, 0.1],
     ["这是一个修真世界，详细世界设定如下：\n1.", 200, 0.9, 0.85, 0.1, 0.1],
     ["怎样创立一家快速盈利的AI公司：\n1.", 200, 0.9, 0.8, 0.1, 0.1],
     ["我问智脑：“三体人发来了信息，告诉我不要回答，这是他们的阴谋吗？”", 200, 0.9, 0.8, 0.1, 0.1],
-    ["我问智脑：“Pytorch比Tensorflow更好用吗？”", 200, 0.9, 0.8, 0.1, 0.1],
+    ["我问编程之神：“Pytorch比Tensorflow更好用吗？”", 200, 0.9, 0.8, 0.1, 0.1],
     ["帝皇是一名极为强大的灵能者，而且还是永生者：一个拥有无穷知识与力量以及使用它们的雄心的不朽存在。根据传说，", 200, 0.9, 0.85, 0.1, 0.1],
     ["Translation\nChinese: 修道之人，最看重的是什么？\nEnglish:", 200, 0.9, 0.5, 0.1, 0.1],
 ]
